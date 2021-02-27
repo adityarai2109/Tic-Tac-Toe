e971ec597cd5971a75453f2dec86cdd35b637ff2
Aditya Rai <adityarai2109@gmail.com>
2021-02-28 01:42:11 +0530

    Proceed implemented and css and responsiveness added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,103 @@
 import React, { useState } from "react";
 import "./App.css";
 import Game from "./components/Game";
+import CloseIcon from "@material-ui/icons/Close";
+import RadioButtonUncheckedIcon from "@material-ui/icons/RadioButtonUnchecked";
 
 function App() {
-  const [player, setPlayer] = useState(true);
+  const [player, setPlayer] = useState(Math.random() >= 0.5);
+  const [names, setNames] = useState(["Player 1 ", "Player 2 "]);
+  const [draw, setDraw] = useState(false);
+
+  const [proceed, setProceed] = useState(true);
+
+  let X = <CloseIcon fontSize="large" />;
+  let O = <RadioButtonUncheckedIcon fontSize="large" />;
+
+  const NamesHandler = (e, i) => {
+    let newNames = [...names];
+    newNames[i] = e;
+    setNames(newNames);
+  };
+
+  const Result = () => {
+    console.log("result");
+    if (draw) {
+      console.log("draw");
+    } else {
+      console.log("player won");
+    }
+    return <div></div>;
+  };
+
   return (
     <div className="App">
       <h1 className="heading">Tic Tac Toe</h1>
-      <div className="content">
-        <Game player={player} setPlayer={setPlayer} />
-        <h3>{player ? "Player 1" : "Player 2"} its your chance</h3>
-      </div>
+      {proceed && (
+        <form className="form">
+          <div className="names">
+            <div className="details">
+              <fieldset>
+                <legend>
+                  <b>
+                    {"\xa0"}Player 1{"\xa0"}
+                  </b>
+                </legend>
+                <p>Enter your name</p>
+                <input
+                  type="text"
+                  className="input"
+                  placeholder="Hello Player 1"
+                  onChange={(e) => NamesHandler(e.target.value, 0)}
+                />
+              </fieldset>
+            </div>
+            <div className="details">
+              <fieldset>
+                <legend>
+                  <b>
+                    {"\xa0"}Player 2{"\xa0"}
+                  </b>
+                </legend>
+                <p>Enter your name</p>
+                <input
+                  type="text"
+                  className="input"
+                  placeholder="Hello Player 2"
+                  onChange={(e) => NamesHandler(e.target.value, 1)}
+                />
+              </fieldset>
+            </div>
+          </div>
+          <h4>
+            Writing name is optional,
+            <br /> but you should write your name
+            <br /> for better experience
+          </h4>
+          <button className="submit" onClick={() => setProceed(false)}>
+            Submit
+          </button>
+        </form>
+      )}
+
+      {!proceed && (
+        <div className="content">
+          <Game
+            player={player}
+            setPlayer={setPlayer}
+            proceed={proceed}
+            setProceed={setProceed}
+            names={names}
+            setNames={setNames}
+            Result={Result}
+          />
+
+          <h3>
+            {player ? `${names[0]} (${X}) ` : `${names[1]} (${O}) `}
+            its your chance
+          </h3>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -5,108 +5,91 @@ import RadioButtonUncheckedIcon from "@material-ui/icons/RadioButtonUnchecked";
 
 export default function Game(props) {
   const [arr, setArr] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [count, setCount] = useState(0);
+
   function Change(index) {
-    props.setPlayer(!props.player);
-    if (props.player) {
-      let newArr = [...arr];
-      newArr[index] = <CloseIcon />;
-      setArr(newArr);
-      //arr[index] = <CloseIcon />;
-    } else {
-      let newArr = [...arr];
-      newArr[index] = <RadioButtonUncheckedIcon />;
-      setArr(newArr);
-      // arr[index] = <RadioButtonUncheckedIcon />;
+    if (arr[index] === "") {
+      props.setPlayer(!props.player);
+      if (props.player) {
+        let newArr = [...arr];
+        newArr[index] = <CloseIcon fontSize="large" />;
+        setArr(newArr);
+      } else {
+        let newArr = [...arr];
+        newArr[index] = <RadioButtonUncheckedIcon fontSize="large" />;
+        setArr(newArr);
+      }
+      setCount(count + 1);
+      console.log(count);
     }
     console.log(arr);
+    if (count === 8) props.Result();
   }
+
   return (
-    <div className="game">
-      <button className="buttons" onClick={() => Change(0)}>
-        {arr[0]}
-      </button>
-      <button className="buttons" onClick={() => Change(1)}>
-        {arr[1]}
-      </button>
-      <button className="buttons" onClick={() => Change(2)}>
-        {arr[2]}
-      </button>
-      <button className="buttons" onClick={() => Change(3)}>
-        {arr[3]}
-      </button>
-      <button className="buttons" onClick={() => Change(4)}>
-        {arr[4]}
-      </button>
-      <button className="buttons" onClick={() => Change(5)}>
-        {arr[5]}
-      </button>
-      <button className="buttons" onClick={() => Change(6)}>
-        {arr[6]}
-      </button>
-      <button className="buttons" onClick={() => Change(7)}>
-        {arr[7]}
-      </button>
-      <button className="buttons" onClick={() => Change(8)}>
-        {arr[8]}
-      </button>
-    </div>
+    <>
+      <div className="game">
+        <div className="button">
+          <button className="buttons" onClick={() => Change(0)}>
+            {arr[0]}
+          </button>
+        </div>
+        <div className="button">
+          <button className="buttons" onClick={() => Change(1)}>
+            {arr[1]}
+          </button>
+        </div>
+        <div className="button">
+          <button className="buttons" onClick={() => Change(2)}>
+            {arr[2]}
+          </button>
+        </div>
+        <div className="button">
+          <button className="buttons" onClick={() => Change(3)}>
+            {arr[3]}
+          </button>
+        </div>
+
+        <div className="button">
+          <button className="buttons" onClick={() => Change(4)}>
+            {arr[4]}
+          </button>
+        </div>
+        <div className="button">
+          <button className="buttons" onClick={() => Change(5)}>
+            {arr[5]}
+          </button>
+        </div>
+        <div className="button">
+          <button className="buttons" onClick={() => Change(6)}>
+            {arr[6]}
+          </button>
+        </div>
+        <div className="button">
+          <button className="buttons" onClick={() => Change(7)}>
+            {arr[7]}
+          </button>
+        </div>
+        <div className="button">
+          <button className="buttons" onClick={() => Change(8)}>
+            {arr[8]}
+          </button>
+        </div>
+      </div>
+      {
+        // count === 9 &&
+        <button
+          className="refresh"
+          onClick={() => {
+            setArr(["", "", "", "", "", "", "", "", ""]);
+            setCount(0);
+            props.setProceed(true);
+            props.setNames(["Player 1 ", "Player 2 "]);
+          }}
+        >
+          Refresh
+        </button>
+      }
+    </>
   );
 }
-
-// export default function Game(props) {
-//   const [moves, setMoves] = useState([
-//     { id: 0, value: false },
-//     { id: 1, value: false },
-//     { id: 2, value: false },
-//     { id: 3, value: false },
-//     { id: 4, value: false },
-//     { id: 5, value: false },
-//     { id: 6, value: false },
-//     { id: 7, value: false },
-//     { id: 8, value: false },
-//   ]);
-//   const [currentButton, setCurrentButton] = useState(0);
-
-//   // moves.forEach((element) => console.log(element.id, element.value));
-
-//   const block = () => {
-//     let button = "";
-
-//     function handleChange(e) {
-//       console.log(e);
-//       // setCurrentButton(e.target.value);
-//       // console.log(currentButton);
-//     }
-
-//     const change = () => {
-//       props.setPlayer(!props.player);
-//       console.log(currentButton);
-
-//       const chance = () => {
-//         let newMoves = [...moves];
-//         newMoves[currentButton].value = true;
-//         setMoves(newMoves);
-//         if (!moves.value) {
-//           if (props.player) {
-//             button = <CloseIcon />;
-//           } else {
-//             button = <RadioButtonUncheckedIcon />;
-//           }
-//         }
-//       };
-//       chance();
-
-//       // console.table(moves);
-//     };
-
-//     return (
-//       <>
-//         <button className="buttons" onClick={change} onChange={handleChange}>
-//           {button}
-//         </button>
-//       </>
-//     );
-//   };
-
-//   return <div className="game">{moves.map(block)}</div>;
-// }
